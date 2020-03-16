824b08ae4dceb3c20ad81a4a1e4346a076055a6a
Ariane4 <abischoff48@gmailcom>
2020-03-16 15:11:49 +0100

    Lev1_1 upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+//Lev1_1_js-vertiefung_objects_intro_europaichen-union
+//-----------------------------------------------
 let europeanUnion = {
     Belgien: "(BE)",
     Griechenland: "(EL)",
@@ -11,3 +13,21 @@ let europeanUnion = {
 }
 console.log(europeanUnion)
 europeanUnion.BeneLuxUnion()
+
+delete europeanUnion.Niederlande
+
+
+// Lev1_2_js-vertiefung_objects_function_alert
+//-----------------------------------------------
+let person = {
+    name: "Ariane",
+    alter: 46,
+    sagNameAlter: function () {
+        alert(this.name + this.alter)
+    }
+}
+console.log(person.sagNameAlter)
+console.log(person.name)
+console.log(person.alter)
+
+//FRAGE: Welchen Fehler habe ich bei dem Befehl "alert"?
